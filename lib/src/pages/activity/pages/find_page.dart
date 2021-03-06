71ce6325a11c57b9da89698fea4212cac7b3a299
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-03-06 20:25:42 +0700

    done layout find page

---
+++ lib/src/pages/activity/pages/find_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:whoru/src/common/styles.dart';
+import 'package:whoru/src/pages/home/widgets/post_card.dart';
+
+class FindPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FindPageState();
+}
+
+class _FindPageState extends State<FindPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: mC,
+        child: Column(
+          children: [
+            SizedBox(height: 4.0),
+            Expanded(
+              child: NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (overscroll) {
+                  overscroll.disallowGlow();
+                  return true;
+                },
+                child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  padding: EdgeInsets.only(top: .0),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return PostCard(
+                      idPost: index.toString(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
