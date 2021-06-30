35350378ec3a88a79805f1d17aaedc5fee01507d
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-07-01 01:30:23 +0700

    [lbc: UI] change bottom navigation

--- lib/src/widgets/image_widget.dart
+++ lib/src/widgets/image_widget.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-import 'package:whoru/src/themes/app_colors.dart';
+import 'package:whoru/src/themes/app_decoration.dart';
 
 class ErrorLoadingImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _size = MediaQuery.of(context).size;
     return Container(
-      decoration: BoxDecoration(
-        color: mCD,
-        boxShadow: [
-          BoxShadow(
-              color: mCL,
-              offset: Offset(3, 3),
-              blurRadius: 3,
-              spreadRadius: -3),
-        ],
-      ),
-      alignment: Alignment.center,
-      child: Icon(
-        Feather.image,
-        color: colorTitle,
-        size: _size.width / 15.0,
-      ),
+      decoration: AppDecoration.buttonActionCircle(context).decoration,
     );
   }
 }
@@ -31,16 +14,7 @@ class PlaceHolderImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: mCD,
-        boxShadow: [
-          BoxShadow(
-              color: mCL,
-              offset: Offset(3, 3),
-              blurRadius: 3,
-              spreadRadius: -3),
-        ],
-      ),
+      decoration: AppDecoration.inputChatDecoration(context).decoration,
     );
   }
 }
